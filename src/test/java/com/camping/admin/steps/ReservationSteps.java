b1a16ee1790d07101ebc953521270e40af9dcd20
suzhanlee <wlscww@kakao.com>
2025-09-10 18:07:29 +0900

    temp: 개발 환경 이동

--- src/test/java/com/camping/admin/steps/ReservationSteps.java
+++ src/test/java/com/camping/admin/steps/ReservationSteps.java
@@ -1,29 +1,65 @@
 package com.camping.admin.steps;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReservationSteps {
+    private int reservationId;
 
-    @Given("사용자가 예약을 했다")
-    public void 사용자가예약을했다() {
-
+    @When("관리자가 로그인을 한다.")
+    public void 관리자가로그인을한다() {
+        RestAssured.given()
+                .when()
+                .post("http://localhost:8080/admin/login")
+                .then()
+                .statusCode(200);
     }
 
-    @When("관리자가 예약 {int}을 취소했다")
-    public void 관리자가예약을취소했다(int arg0) {
-
+    @When("관리자가 예약 상태를 변경한다.")
+    public void 관리자가예약상태를변경한다() {
+        reservationId = 1;
+        RestAssured.given()
+                .when()
+                .patch("http://localhost:8080/admin/reservations/" + reservationId + "/status")
+                .then()
+                .statusCode(200);
     }
 
-    @Then("예약은 취소 상태다")
-    public void 예약은취소상태다() {
+    @Then("예약 상태가 변경된다.")
+    public void 예약상태가변경된다() {
+        ExtractableResponse<Response> response = RestAssured.given()
+                .when()
+                .get("http://localhost:8080/admin/reservations")
+                .then()
+                .statusCode(200)
+                .extract();
 
-    }
+        String status = response.jsonPath().getList("$").stream()
+                .filter(s -> ((Integer) ((Map<String, Object>) s).get("id")) == 1)
+                .findFirst()
+                .toString();
 
-    @And("해당 자원은 다시 예약 가능하다")
-    public void 해당자원은다시예약가능하다() {
-
+        assertThat(status).contains("CONFIRMED");
     }
 }
+
+
+//[
+//        {
+//        "id": 1,
+//        "customerName": "�솉湲몃룞",
+//        "startDate": "2025-09-10",
+//        "endDate": "2025-09-11",
+//        "status": "CONFIRMED",
+//        "campsiteSiteNumber": "A-01",
+//        "reservationDate": "2025-09-10"
+//        },
+//]
