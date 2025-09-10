300b6e2db13a9d78b2e2db8004b32dd559cb7181
etff <wish2rich@gmail.com>
2025-09-11 01:46:57 +0900

    feat: reservation step 추가

--- src/test/java/com/camping/admin/CucumberTestRunner.java
+++ src/test/java/com/camping/admin/CucumberTestRunner.java
@@ -1,11 +1,7 @@
 package com.camping.admin;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @SelectClasspathResource("features")
