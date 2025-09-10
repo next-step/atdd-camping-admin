85aadcd19e25e8ecf5a964691fc7740c139408ea
Chris (손영철) <chris8282@nol-universe.com>
2025-09-10 22:21:18 +0900

    test: Cucumber test 실행 전 RestAssured 셋업 기능 추가

---
+++ src/test/java/com/camping/admin/steps/CucumberTestHooks.java
@@ -0,0 +1,30 @@
+package com.camping.admin.steps;
+
+import io.cucumber.java.BeforeAll;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CucumberTestHooks {
+
+    @BeforeAll
+    public static void setupRestAssured() {
+        var adminApiUrl = "http://localhost:8080";
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(adminApiUrl)
+            .setContentType(ContentType.JSON)
+            .addFilters(List.of(
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter()
+            ))
+            .build();
+
+        log.info("RestAssured request specification initialized.");
+    }
+}
