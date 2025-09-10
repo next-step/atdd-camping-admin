1c5becde5e097afc5184203bf40bb814d667ac59
박준형 <park.jh@altsoft.kr>
2025-09-10 12:33:37 +0900

    test: cucumber 테스트 환경 구성

---
+++ src/test/java/com/camping/CucumberTestRunner.java
@@ -0,0 +1,10 @@
+package com.camping;
+
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasspathResource("features")
+public class CucumberTestRunner {
+
+}
