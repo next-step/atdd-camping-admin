e54a88e577fd1502f011ddce84282d749d2f88bc
suzhanlee <wlscww@kakao.com>
2025-09-10 12:10:27 +0900

    feat: cucumber 기본 설정 구현

---
+++ src/test/java/com/camping/admin/steps/ReservationSteps.java
@@ -0,0 +1,29 @@
+package com.camping.admin.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ReservationSteps {
+
+    @Given("사용자가 예약을 했다")
+    public void 사용자가예약을했다() {
+
+    }
+
+    @When("관리자가 예약 {int}을 취소했다")
+    public void 관리자가예약을취소했다(int arg0) {
+
+    }
+
+    @Then("예약은 취소 상태다")
+    public void 예약은취소상태다() {
+
+    }
+
+    @And("해당 자원은 다시 예약 가능하다")
+    public void 해당자원은다시예약가능하다() {
+
+    }
+}
