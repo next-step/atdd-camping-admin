8b235dd250de8ba8e88ccc6d30293355a5315c25
parkchu <parkjoohan0603@gmail.com>
2025-09-12 00:01:51 +0900

    refactor: 인수 테스트 규칙 README.md 파일 추가

--- src/test/java/com/camping/admin/steps/ResponseSteps.java
+++ src/test/java/com/camping/admin/steps/ResponseSteps.java
@@ -7,13 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static com.camping.admin.hooks.TokenHook.testContext;
 
 public class ResponseSteps {
-    @Then("{int} 코드로 응답 한다.")
+    @Then("{int} 코드로 응답 된다.")
     public void failWithStatusCode(int statusCode) {
         var response = testContext.getResponse();
         assertThat(response.getStatusCode()).isEqualTo(statusCode);
     }
 
-    @Then("{string} 메시지가 응답 한다.")
+    @Then("{string} 메시지가 응답 된다.")
     public void responseMessage(String message) {
         var response = testContext.getResponse();
         assertThat(response.jsonPath().getString("message")).isEqualTo(message);
