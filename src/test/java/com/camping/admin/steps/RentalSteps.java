07e5407c1011d9d09c4314d37a97acddfb59a077
suzhanlee <wlscww@kakao.com>
2025-09-11 12:03:18 +0900

    test: 상품이 렌탈 상품이 아니라면, 관리자는 대여 기록을 생성할 수 없는 인수테스트 생성

--- src/test/java/com/camping/admin/steps/RentalSteps.java
+++ src/test/java/com/camping/admin/steps/RentalSteps.java
@@ -1,47 +1,67 @@
 package com.camping.admin.steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 import java.util.Map;
+import java.util.Optional;
 
-import static com.camping.admin.steps.RentalTestFixture.대여기록목록조회;
-import static com.camping.admin.steps.StepContext.getAccessToken;
-import static com.camping.admin.steps.StepContext.getRequestSpecification;
+import static com.camping.admin.steps.RentalTestFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RentalSteps {
-    private int rentalId;
+    private Long rentalId;
+    private int statusCode;
 
-    @When("관리자는 특정 상품의 수량 만큼 대여 기록을 작성한다.")
-    public void 관리자는특정상품의수량만큼대여기록을작성한다() {
-        ExtractableResponse<Response> response = RestAssured.given()
-                .spec(getRequestSpecification())
-                .header("Authorization", "Bearer " + getAccessToken())
-                .body("{\"reservationId\": 1, \"productId\": 1, \"quantity\": 2}")
-                .when()
-                .post("/admin/rentals")
-                .then()
-                .statusCode(201)
-                .extract();
+    @When("관리자는 특정 상품의 수량 만큼 대여를 시도한다.")
+    public void 관리자는특정상품의수량만큼대여를시도한다(DataTable dataTable) {
+        ExtractableResponse<Response> response = 대여기록작성요청(extractRentalBodyFromDataTable(dataTable));
+        if (response.statusCode() == 201) {
+            rentalId = response.jsonPath().getLong("id");
+        }
+        statusCode = response.statusCode();
+    }
 
-        rentalId = response.jsonPath().getInt("id");
+    private Map<String, Integer> extractRentalBodyFromDataTable(DataTable dataTable) {
+        Map<String, String> data = dataTable.asMap(String.class, String.class);
+        return Map.of(
+                "reservationId", Integer.parseInt(data.get("reservationId")),
+                "productId", Integer.parseInt(data.get("productId")),
+                "quantity", Integer.parseInt(data.get("quantity"))
+        );
     }
 
     @Then("대여 기록이 생성된다.")
     public void 대여기록이생성된다() {
-        Map<String, Object> rental = 대여기록목록조회(rentalId);
+        assertThat(statusCode).isEqualTo(201);
+        Map<String, Object> rental = 특정대여기록조회(rentalId);
         assertThat(rental.get("isReturned")).isEqualTo(false);
     }
 
     @And("상품의 수량이 대여 기록의 수량 만큼 줄어든다.")
     public void 상품의수량이대여기록의수량만큼줄어든다() {
-        Map<String, Object> rental = 대여기록목록조회(rentalId);
+        Map<String, Object> rental = 특정대여기록조회(rentalId);
         assertThat(rental.get("quantity")).isEqualTo(2);
+    }
+
+    @Then("대여에 실패한다.")
+    public void 대여에실패한다() {
+        assertThat(statusCode).isEqualTo(500);
+    }
+
+    @Then("대여 기록이 생성되지 않는다.")
+    public void 대여기록이생성되지않는다(DataTable dataTable) {
+        long productId = Long.parseLong(dataTable.asMap().get("productId"));
+
+        ExtractableResponse<Response> response = 대여기록목록조회();
+        Optional<Map> rentalRecord = response.jsonPath().getList(".", Map.class).stream()
+                .filter(record -> ((Number) record.get("productId")).longValue() == productId)
+                .findFirst();
+
+        assertThat(rentalRecord).isEmpty();
     }
 }
--- src/test/java/com/camping/admin/steps/RentalTestFixture.java
+++ src/test/java/com/camping/admin/steps/RentalTestFixture.java
@@ -6,21 +6,38 @@ import io.restassured.response.Response;
 
 import java.util.Map;
 
+import static com.camping.admin.steps.StepContext.getAccessToken;
+import static com.camping.admin.steps.StepContext.getRequestSpecification;
+
 public class RentalTestFixture {
-    public static Map<String, Object> 대여기록목록조회(long rentalId) {
-        ExtractableResponse<Response> response = RestAssured.given()
-                .spec(StepContext.getRequestSpecification())
+    public static Map<String, Object> 특정대여기록조회(long rentalId) {
+        ExtractableResponse<Response> response = 대여기록목록조회();
+        Map<String, Object> rental = (Map<String, Object>) response.jsonPath().getList("$").stream()
+                .filter(r -> ((Integer) ((Map<String, Object>) r).get("id")) == rentalId)
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("대여 기록을 찾을 수 없습니다."));
+        return rental;
+    }
+
+    public static ExtractableResponse<Response> 대여기록목록조회() {
+        return RestAssured.given()
+                .spec(getRequestSpecification())
                 .header("Authorization", "Bearer " + StepContext.getAccessToken())
                 .when()
                 .get("/admin/rentals")
                 .then()
                 .statusCode(200)
                 .extract();
+    }
 
-        Map<String, Object> rental = (Map<String, Object>) response.jsonPath().getList("$").stream()
-                .filter(r -> ((Integer) ((Map<String, Object>) r).get("id")) == rentalId)
-                .findFirst()
-                .orElseThrow(() -> new AssertionError("대여 기록을 찾을 수 없습니다."));
-        return rental;
+    public static ExtractableResponse<Response> 대여기록작성요청(Map<String, Integer> body) {
+        return RestAssured.given()
+                .spec(getRequestSpecification())
+                .header("Authorization", "Bearer " + getAccessToken())
+                .body(body)
+                .when()
+                .post("/admin/rentals")
+                .then()
+                .extract();
     }
 }
