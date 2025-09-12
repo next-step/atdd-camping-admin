c8680cefb2799b2f4d21b16966e3b59fb2df8ba1
parkseryu <pjh971212@gmail.com>
2025-09-12 18:42:45 +0900

    refactor: 데이터 초기화 로직 Helper 클래스로 분리

--- src/test/java/com/camping/admin/steps/hooks/Hooks.java
+++ src/test/java/com/camping/admin/steps/hooks/Hooks.java
@@ -1,43 +1,17 @@
 package com.camping.admin.steps.hooks;
 
-import static io.restassured.RestAssured.given;
-
 import com.camping.admin.config.RequestSpecFactory;
 import com.camping.admin.context.CommonContext;
+import com.camping.admin.helper.TestApiHelper;
 import io.cucumber.java.Before;
-import java.util.Map;
 
 public class Hooks {
+
     @Before
     public void setUp() {
         CommonContext.setRequestSpec(RequestSpecFactory.create());
-        authenticate();
-        cleanupDatabase();
-    }
-
-    private void authenticate() {
-        String adminToken = given()
-                .spec(CommonContext.getRequestSpec())
-                .contentType("application/json")
-                .body(Map.of("username", "admin", "password", "admin123"))
-                .when()
-                .post("/auth/login")
-                .then()
-                .statusCode(200)
-                .extract()
-                .cookie("AUTH_TOKEN");
-
+        String adminToken = TestApiHelper.authenticateAndGetToken();
         CommonContext.setAdminToken(adminToken);
+        TestApiHelper.cleanupDatabase(adminToken);
     }
-
-    private void cleanupDatabase() {
-        given().spec(CommonContext.getRequestSpec())
-                .header("Authorization", "Bearer " + CommonContext.getAdminToken())
-                .when()
-                .post("/api/admin/reset-db")
-                .then()
-                .statusCode(200);
-    }
-
-
 }
