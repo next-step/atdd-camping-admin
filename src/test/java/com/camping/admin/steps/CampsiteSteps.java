0b866579cbb34efa034595649c61e4f69ae41573
parkseryu <pjh971212@gmail.com>
2025-09-12 12:46:39 +0900

    test: 예외/엣지 케이스 시나리오 확장

--- src/test/java/com/camping/admin/steps/CampsiteSteps.java
+++ src/test/java/com/camping/admin/steps/CampsiteSteps.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
@@ -15,24 +16,25 @@ import java.util.Map;
 public class CampsiteSteps {
 
     private Response response;
-    private String generatedSiteNumber;
 
-    @When("고유한 사이트 번호와 설명 {string}, 최대 인원 {int}명으로 캠프사이트 생성을 요청한다")
-    public void requestCampsiteCreation(String description, int maxPeople) {
+    @Given("{string} 사이트 번호로 캠프사이트가 이미 존재한다")
+    public void campsiteWithSiteNumberAlreadyExists(String siteNumber) {
+        Map<String, Object> existingCampsite = new HashMap<>();
+        existingCampsite.put("siteNumber", siteNumber);
+        existingCampsite.put("description", "기존 캠프사이트");
+        existingCampsite.put("maxPeople", 4);
+
+        authenticatedRequest.body(existingCampsite)
+                .when()
+                .post("/admin/campsites")
+                .then()
+                .statusCode(201);
+    }
+
     @When("사이트 번호 {string}, 설명 {string}, 최대 인원 {int}명으로 캠프사이트 생성을 요청한다")
     public void requestCampsiteCreationWithSiteNumber(String siteNumber, String description, int maxPeople) {
         Map<String, Object> campsiteDetails = new HashMap<>();
-        generatedSiteNumber = "A-" + System.currentTimeMillis();
-        campsiteDetails.put("siteNumber", generatedSiteNumber);
         campsiteDetails.put("siteNumber", siteNumber);
-        campsiteDetails.put("description", description);
-        campsiteDetails.put("maxPeople", maxPeople);
-
-        response = authenticatedRequest.body(campsiteDetails)
-                .when()
-                .post("/admin/campsites");
-    }
-
         campsiteDetails.put("description", description);
         campsiteDetails.put("maxPeople", maxPeople);
 
@@ -46,9 +48,11 @@ public class CampsiteSteps {
         response.then().statusCode(201);
     }
 
-    @And("생성된 캠프사이트의 사이트 번호는 생성 시 사용한 번호이다")
-    public void theCampsiteSiteNumberIsTheGeneratedOne() {
-        response.then().body("siteNumber", equalTo(generatedSiteNumber));
+    @Then("캠프사이트 생성에 실패하고 오류 메시지를 받는다")
+    public void campsiteCreationFailsWithErrorMessage() {
+        response.then().assertThat().statusCode(409);
+    }
+
     @And("생성된 캠프사이트의 사이트 번호는 {string}이다")
     public void theCampsiteSiteNumberIs(String siteNumber) {
         response.then().body("siteNumber", equalTo(siteNumber));
