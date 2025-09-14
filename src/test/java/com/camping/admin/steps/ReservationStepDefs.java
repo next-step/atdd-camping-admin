9bf470617cd34a03fa6966b638ef586b736cf673
MoonJeWoong <munjin0201@naver.com>
2025-09-14 23:59:45 +0900

    test: 인수테스트 작성-취소 상태인 사용자 예약은 체크인 상태로 변경할 수 없다

--- src/test/java/com/camping/admin/steps/ReservationStepDefs.java
+++ src/test/java/com/camping/admin/steps/ReservationStepDefs.java
@@ -2,10 +2,13 @@ package com.camping.admin.steps;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.camping.admin.support.ApiHelper;
 import com.camping.admin.support.CommonContext;
-import com.camping.admin.support.RequestSpecFactory;
+import io.cucumber.core.options.CurlOption.HttpMethod;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -32,37 +35,27 @@ public class ReservationStepDefs {
     public void 사용자가예약을했다() {
         reservationId = 1L;
 
+        // 여러 테스트에서 사용해도 해당 예약 상태가 동일하도록 초기화 수행
+        String url = String.format("/admin/reservations/%d/status", reservationId);
         Map<String, Object> body = Map.of("status", "CONFIRMED");
-        given().spec(CommonContext.requestSpec)
-                .header("Authorization", "Bearer " + CommonContext.adminToken)
-                .body(body)
-                .when()
-                .patch("/admin/reservations/{reservationId}/status", reservationId)
-                .then().log().all()
-                .statusCode(200)
-                .extract();
+
+        ApiHelper.request(HttpMethod.PATCH, url, body)
+                .then().statusCode(200);
     }
 
     @When("관리자가 예약을 취소했다")
     public void 관리자가예약을취소했다() {
-
+        String url = String.format("/admin/reservations/%d/status", reservationId);
         Map<String, Object> body = Map.of("status", "CANCELLED");
-        given().spec(CommonContext.requestSpec)
-                .header("Authorization", "Bearer " + CommonContext.adminToken)
-                .body(body)
-                .when()
-                .patch("/admin/reservations/{reservationId}/status", reservationId)
-                .then().log().all()
-                .statusCode(200)
-                .extract();
+
+        ApiHelper.request(HttpMethod.PATCH, url, body);
     }
 
     @Then("예약은 취소 상태다")
     public void 예약은취소상태다() {
-        ExtractableResponse<Response> reservationsResponse = given()
-                .cookie("AUTH_TOKEN", CommonContext.adminToken)
-                .when()
-                .get("/admin/reservations")
+        String url = "/admin/reservations";
+
+        ExtractableResponse<Response> reservationsResponse = ApiHelper.request(HttpMethod.GET, url, null)
                 .then().statusCode(200)
                 .extract();
 
@@ -71,31 +64,34 @@ public class ReservationStepDefs {
         assertThat(status).isEqualTo("CANCELLED");
     }
 
-    @When("관리자가 예약을 체크인 상태로 변경을 요청한다")
-    public void 관리자가예약을체크인상태로변경을요청한다() {
-
-        Map<String, Object> body = Map.of("status", "CHECKED_IN");
-        given().spec(CommonContext.requestSpec)
-                .header("Authorization", "Bearer " + CommonContext.adminToken)
-                .body(body)
-                .when()
-                .patch("/admin/reservations/{reservationId}/status", reservationId)
-                .then().log().all()
-                .statusCode(200)
-                .extract();
-    }
-
     @Then("예약은 체크인 상태다")
     public void 예약은체크인상태다() {
-        ExtractableResponse<Response> reservationsResponse = given()
-                .cookie("AUTH_TOKEN", CommonContext.adminToken)
-                .when()
-                .get("/admin/reservations")
+        String url = "/admin/reservations";
+
+        ExtractableResponse<Response> reservationsResponse = ApiHelper.request(HttpMethod.GET, url, null)
                 .then().statusCode(200)
                 .extract();
 
         String status = reservationsResponse.jsonPath().getString(String.format("find {it.id == %d}.status", reservationId));
         assertNotNull(status, "취소 예약 대상을 조회하지 못했습니다.");
         assertThat(status).isEqualTo("CHECKED_IN");
+    }
+
+    @When("관리자가 예약을 체크인 상태로 변경한다")
+    public void 관리자가예약을체크인상태로변경한다() {
+        Map<String, Object> body = Map.of("status", "CHECKED_IN");
+        String url = String.format("/admin/reservations/%d/status", reservationId);
+
+        CommonContext.lastResponse = ApiHelper.request(HttpMethod.PATCH, url, body);
+    }
+
+    @And("예약 상태 변경이 실패한다")
+    public void 예약상태변경이실패한다() {
+        CommonContext.lastResponse.then().statusCode(allOf(greaterThanOrEqualTo(400), lessThan(600)));
+    }
+
+    @Then("변경 요청이 성공한다")
+    public void 변경요청이성공한다() {
+        CommonContext.lastResponse.then().statusCode(200);
     }
 }
