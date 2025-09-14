5f509e9862735fdd990dbc1c51bd6d4b34737f91
MoonJeWoong <munjin0201@naver.com>
2025-09-14 17:50:54 +0900

    test: 인수테스트 작성을 위한 cucumber 테스트 러너 클래스 작성

---
+++ src/test/java/features/CucumberTest.java
@@ -0,0 +1,9 @@
+package features;
+
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasspathResource( "features")
+public class CucumberTest {
+}
