0d45cfe0b5a420cfca1c22218f985f7426ca40fc
parkchu <parkjoohan0603@gmail.com>
2025-09-16 22:08:52 +0900

    feature: experience.md 파일 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,12 +30,12 @@ values
   (4, '박민수', DATEADD('DAY', -25, current_date), DATEADD('DAY', -24, current_date), 1001, '010-7777-8888', 'CONFIRMED', DATEADD('DAY', -26, current_date), 'R00004', DATEADD('DAY', -26, current_timestamp)),
   (5, '최수정', DATEADD('DAY', -21, current_date), DATEADD('DAY', -19, current_date), 1000, '010-9999-0000', 'CONFIRMED', DATEADD('DAY', -22, current_date), 'R00005', DATEADD('DAY', -22, current_timestamp)),
   (6, '정하늘', DATEADD('DAY', -18, current_date), DATEADD('DAY', -17, current_date), 1001, '010-2222-3333', 'CONFIRMED', DATEADD('DAY', -19, current_date), 'R00006', DATEADD('DAY', -19, current_timestamp)),
-  (7, '오세훈', DATEADD('DAY', -15, current_date), DATEADD('DAY', -14, current_date), 1000, '010-4444-5555', 'CONFIRMED', DATEADD('DAY', -16, current_date), 'R00007', DATEADD('DAY', -16, current_timestamp)),
+  (7, '오세훈', DATEADD('DAY', -15, current_date), DATEADD('DAY', -14, current_date), 1000, '010-4444-5555', 'REJECTED', DATEADD('DAY', -16, current_date), 'R00007', DATEADD('DAY', -16, current_timestamp)),
   (8, '유지민', DATEADD('DAY', -12, current_date), DATEADD('DAY', -11, current_date), 1001, '010-6666-7777', 'CONFIRMED', DATEADD('DAY', -13, current_date), 'R00008', DATEADD('DAY', -13, current_timestamp)),
-  (9, '선우진', DATEADD('DAY', -9, current_date), DATEADD('DAY', -8, current_date), 1000, '010-1212-3434', 'CONFIRMED', DATEADD('DAY', -10, current_date), 'R00009', DATEADD('DAY', -10, current_timestamp)),
+  (9, '선우진', DATEADD('DAY', -9, current_date), DATEADD('DAY', -8, current_date), 1000, '010-1212-3434', 'REJECTED', DATEADD('DAY', -10, current_date), 'R00009', DATEADD('DAY', -10, current_timestamp)),
   (10, '배수아', DATEADD('DAY', -6, current_date), DATEADD('DAY', -5, current_date), 1001, '010-5656-7878', 'CONFIRMED', DATEADD('DAY', -7, current_date), 'R00010', DATEADD('DAY', -7, current_timestamp)),
-  (11, '고다빈', DATEADD('DAY', -3, current_date), DATEADD('DAY', -2, current_date), 1000, '010-9090-1010', 'CONFIRMED', DATEADD('DAY', -4, current_date), 'R00011', DATEADD('DAY', -4, current_timestamp)),
-  (12, '한도윤', DATEADD('DAY', -1, current_date), current_date, 1001, '010-2323-4545', 'CONFIRMED', DATEADD('DAY', -2, current_date), 'R00012', DATEADD('DAY', -2, current_timestamp));
+  (11, '고다빈', DATEADD('DAY', -3, current_date), DATEADD('DAY', -2, current_date), 1000, '010-9090-1010', 'CANCELLED', DATEADD('DAY', -4, current_date), 'R00011', DATEADD('DAY', -4, current_timestamp)),
+  (12, '한도윤', DATEADD('DAY', -1, current_date), current_date, 1001, '010-2323-4545', 'CANCELLED', DATEADD('DAY', -2, current_date), 'R00012', DATEADD('DAY', -2, current_timestamp));
 
 -- Sales Records
 insert into sales_records (id, product_id, quantity, total_price, created_at) values
