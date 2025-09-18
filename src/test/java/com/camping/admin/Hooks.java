e0150f75f498ae96ff22779a62c33a2dc362e978
etff <wish2rich@gmail.com>
2025-09-19 05:26:30 +0900

    refactor: 로그인 훅 추가

--- src/test/java/com/camping/admin/Hooks.java
+++ src/test/java/com/camping/admin/Hooks.java
@@ -3,11 +3,29 @@ package com.camping.admin;
 import com.camping.admin.context.ScenarioContext;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.Map;
 
 public class Hooks {
     @Before
     public void beforeScenario() {
         ScenarioContext.clear();
+
+        Response response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(Map.of("username", "admin", "password", "admin123"))
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        String token = response.jsonPath().getString("accessToken");
+        ScenarioContext.get().set("accessToken", token);
     }
 
     @After
