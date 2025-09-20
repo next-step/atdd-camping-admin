b3650a35804c1373b3a86622fda3345a01f6d5a4
joyDaheeCha <dahee.cha@kurlycorp.com>
2025-09-20 18:48:11 +0900

    refactor: request 객체 생성용 helper 클래스 및 request 전송용 helper 클래스 추가하라

--- src/test/java/com/camping/admin/steps/Hooks.java
+++ src/test/java/com/camping/admin/steps/Hooks.java
@@ -7,6 +7,9 @@ import io.cucumber.java.BeforeAll;
 import static com.camping.admin.helper.RequestSpecFactory.getRequestSpec;
 import static com.camping.admin.helper.TokenFactory.getAdminToken;
 
+/**
+ * 비즈니스 로직에 영향을 주지 않는 공통 세팅 (예. 토큰 세팅)
+ */
 public class Hooks {
 
     @Before
--- src/test/java/com/camping/admin/steps/RentalSteps.java
+++ src/test/java/com/camping/admin/steps/RentalSteps.java
@@ -7,12 +7,9 @@ import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 
-import java.util.HashMap;
-
-import static com.camping.admin.helper.CommonContext.adminToken;
-import static com.camping.admin.helper.CommonContext.requestSpec;
-import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
+import static com.camping.admin.helper.RequestFactory.createRentalRequest;
+import static com.camping.admin.helper.RequestSender.get;
+import static com.camping.admin.helper.RequestSender.post;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -37,23 +34,13 @@ public class RentalSteps {
 
     @When("사용자가 제품을 대여한다")
     public void 사용자가_제품을_대여한다() {
-        var request = new HashMap<String, Object>();
-        request.put("reservationId", reservationId);
-        request.put("productId", productId);
-        request.put("quantity", 1L);
-
-        lastResponse = given().spec(requestSpec)
-                .contentType(JSON)
-                .header("Authorization", "Bearer " + adminToken)
-                .body(request)
-                .when()
-                .post("/admin/rentals");
+        lastResponse = post("/admin/rentals",
+                createRentalRequest(reservationId, productId, 1L));
     }
 
     @Then("대여에 성공했다")
     public void 대여에_성공했다() {
         lastResponse.then()
-                .log().all()
                 .statusCode(CREATED.value());
     }
 
@@ -63,28 +50,23 @@ public class RentalSteps {
                 .body("id", notNullValue());
     }
 
-    @And("대여된 제품의 재고가 감소했다")
-    public void 제품_재고가_감소된다() {
-        var response = given()
-                .contentType(JSON)
-                .header("Authorization", "Bearer " + adminToken)
-                .when()
-                .get("/admin/products")
-                .then()
-                .log().all()
-                .extract();
-
-        // productId에 해당하는 제품의 stockQuantity 확인
-        var actualStockQuantity = response.jsonPath()
-                .getInt("find { it.id == " + productId + " }.stockQuantity");
-
-        assertThat(actualStockQuantity).isEqualTo(19);
-    }
-
     @And("대여 상태가 '대여중'이다") // TODO: 3단계 리팩터링에서 대여중과 false를 매핑시키는 enum 추가하기
     public void 대여상태가_대여중_이다() {
         lastResponse.then()
                 .body("isReturned", equalTo(false));
     }
 
+    @And("대여된 제품의 재고가 감소했다")
+    public void 제품_재고가_감소된다() {
+        var products = get("/admin/products")
+                .then()
+                .log().all()
+                .extract();
+
+        // productId에 해당하는 제품의 stockQuantity 확인
+        var actualStockQuantity = products.jsonPath()
+                .getInt("find { it.id == " + productId + " }.stockQuantity");
+
+        assertThat(actualStockQuantity).isEqualTo(19);
+    }
 }
