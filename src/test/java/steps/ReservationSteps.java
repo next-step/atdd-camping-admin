2bd66da16fbc0fccfee8a3b0d099edd1f3f2a6ea
who-is-hu <dydgndlaek97@gmail.com>
2025-09-23 22:10:16 +0900

    refactor: 상태변경을 파라미터로 받게 변경

--- src/test/java/steps/ReservationSteps.java
+++ src/test/java/steps/ReservationSteps.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Map;
 
@@ -16,19 +17,19 @@ public class ReservationSteps {
     long reservationId;
     ExtractableResponse<Response> response;
 
-    @Given("사용자가 예약을 했다.")
+    @Given("사용자가 예약을 했다")
     public void 사용자가예약을했다() {
         // 기본 데이터 사용
         reservationId = 1;
     }
 
-    @When("관리자가 예약을 취소한다.")
-    public void 관리자가예약을취소한다() {
+    @When("관리자가 예약을 {string} 상태로 변경한다")
+    public void 관리자가예약상태를변경한다(String status) {
         String accessToken = AuthHooks.getAccessToken();
-        response = sendChangeStatus(accessToken, reservationId, Map.of("status", "CANCELLED"));
+        response = sendChangeStatus(accessToken, reservationId, Map.of("status", status));
     }
 
-    @Then("예약은 취소된다.")
+    @Then("예약은 취소된다")
     public void 예약은취소된다() {
         String status = response.jsonPath().getString("status");
         long reservationId = response.jsonPath().getInt("id");
--- src/test/resources/feature/reservation.feature
+++ src/test/resources/feature/reservation.feature
@@ -1,5 +1,8 @@
-Feature: 예약 취소
+Feature: 예약 상태 변경
   Scenario: 관리자가 예약을 취소한다
+    Given 사용자가 예약을 했다
+    When 관리자가 예약을 "CANCELLED" 상태로 변경한다
+    Then 예약은 취소된다
     Given 사용자가 예약을 했다.
     When 관리자가 예약을 취소한다.
     Then 예약은 취소된다.
