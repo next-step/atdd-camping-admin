faeab9671b9a6af8ea809f73005de59614f9000e
etff <wish2rich@gmail.com>
2025-09-30 01:00:47 +0900

    refactor: 리뷰반영, after 이후 케이스 제거

--- src/test/java/com/camping/admin/Hooks.java
+++ src/test/java/com/camping/admin/Hooks.java
@@ -2,19 +2,13 @@ package com.camping.admin;
 
 import com.camping.admin.helpers.AuthHelper;
 import com.camping.admin.helpers.ContextHelper;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
     @Before
     public void beforeScenario() {
-        ContextHelper.clearContext();
-        AuthHelper.performAdminLogin();
-    }
-
-    @After
-    public void afterScenario() {
         AuthHelper.clearTokens();
         ContextHelper.clearContext();
+        AuthHelper.performAdminLogin();
     }
 }
