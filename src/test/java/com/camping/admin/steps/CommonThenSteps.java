68d1656fef27c7c99eb5294a455a03520831be93
Sooyoung <kswim8914@gmail.com>
2025-10-02 17:14:27 +0900

    fix: Step1 피드백 적용 - feature 파일의 내용을 비개발적 용어로 수정

--- src/test/java/com/camping/admin/steps/CommonThenSteps.java
+++ src/test/java/com/camping/admin/steps/CommonThenSteps.java
@@ -12,6 +12,17 @@ import static com.camping.admin.steps.ReservationListSteps.lastResponse;
 
 public class CommonThenSteps {
 
+    @Then("예약 목록을 응답 받았다.")
+    public void 예약목록을응답받았다() {
+        Response res = lastResponse.then().extract().response();
+        if (res.statusCode() != 200) {
+            System.out.println("[ATDD] >>> Unexpected status: " + res.statusCode());
+            System.out.println("[ATDD] >>> Body: " + res.asString());
+            System.out.println("[ATDD] >>> Headers: " + res.getHeaders());
+        }
+        lastResponse.then().statusCode(200);
+    }
+
     @And("응답 본문은 유효한 예약 목록이다.")
     public void 응답본문은유효한예약목록이다() {
         // 루트가 JSON 배열인지 확인한다
@@ -26,16 +37,5 @@ public class CommonThenSteps {
                     .body("[0].id", notNullValue())
                     .body("[0].status", notNullValue());
         }
-    }
-
-    @Then("응답 상태코드는 {int}이다.")
-    public void 응답상태코드는이다(int code) {
-        Response res = lastResponse.then().extract().response();
-        if (res.statusCode() != code) {
-            System.out.println("[ATDD] >>> Unexpected status: " + res.statusCode());
-            System.out.println("[ATDD] >>> Body: " + res.asString());
-            System.out.println("[ATDD] >>> Headers: " + res.getHeaders());
-        }
-        lastResponse.then().statusCode(code);
     }
 }
