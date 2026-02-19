b61f80b67ba4186e001a60fbbbc7dffbca426624
tongnamuu <thban33@naver.com>
2026-02-20 00:38:05 +0900

    campsite-management.feature

--- src/test/java/com/camping/admin/steps/campsiteSteps.java
+++ src/test/java/com/camping/admin/steps/campsiteSteps.java
@@ -1,32 +1,64 @@
 package com.camping.admin.steps;
 
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
-public class    campsiteSteps {
-    @Then("응답 상태 코드는 {int}이다")
-    public void 응답상태코드는이다(int arg0) {
-        System.out.println("응답 상태 코드는 " + arg0 + "입니다.");
+public class campsiteSteps {
+
+    @Given("캠프사이트 {int}개가 등록되어 있다")
+    public void 캠프사이트N개가등록되어있다(int count) {
+        System.out.println("캠프사이트 " + count + "개 등록");
     }
 
-    @And("캠프사이트 목록이 반환된다")
-    public void 캠프사이트목록이반환된다() {
-        System.out.println("캠프사이트 목록이 반환되었습니다.");
+    @Given("캠프사이트가 등록되어 있다")
+    public void 캠프사이트가등록되어있다() {
+        System.out.println("캠프사이트 등록");
     }
 
-    @And("등록된 캠프사이트의 사이트 번호는 {string}이다")
-    public void 등록된캠프사이트의사이트번호는이다(String arg0) {
-        System.out.println("등록된 캠프사이트의 사이트 번호는 " + arg0 + "입니다.");
+    @When("^캠프사이트 목록을 조회한다 \\(GET \"/admin/campsites\"\\)$")
+    public void 캠프사이트목록을조회한다() {
+        System.out.println("GET /admin/campsites 요청");
     }
 
-    @And("등록된 캠프사이트의 최대 인원은 {int}이다")
-    public void 등록된캠프사이트의최대인원은이다(int arg0) {
-        System.out.println("등록된 캠프사이트의 최대 인원은 " + arg0 + "입니다.");
+    @When("^새로운 캠프사이트를 등록한다 \\(POST \"/admin/campsites\"\\)$")
+    public void 새로운캠프사이트를등록한다() {
+        System.out.println("POST /admin/campsites 요청");
     }
 
-    @And("캠프사이트의 설명은 {string}이다")
-    public void 캠프사이트의설명은이다(String arg0) {
-        System.out.println("캠프사이트의 설명은 " + arg0 + "입니다.");
+    @When("^캠프사이트 정보를 수정한다 \\(PUT \"/admin/campsites/\\{id\\}\"\\)$")
+    public void 캠프사이트정보를수정한다() {
+        System.out.println("PUT /admin/campsites/{id} 요청");
+    }
+
+    @Then("^조회에 성공한다 \\((\\d+)\\)$")
+    public void 조회에성공한다(int statusCode) {
+        System.out.println("응답 상태 코드: " + statusCode);
+    }
+
+    @Then("^캠프사이트가 생성된다 \\((\\d+)\\)$")
+    public void 캠프사이트가생성된다(int statusCode) {
+        System.out.println("응답 상태 코드: " + statusCode);
+    }
+
+    @Then("^수정에 성공한다 \\((\\d+)\\)$")
+    public void 수정에성공한다(int statusCode) {
+        System.out.println("응답 상태 코드: " + statusCode);
+    }
+
+    @And("캠프사이트 {int}개가 반환된다")
+    public void 캠프사이트N개가반환된다(int count) {
+        System.out.println("반환된 캠프사이트 수: " + count);
+    }
+
+    @And("생성된 캠프사이트 정보가 반환된다")
+    public void 생성된캠프사이트정보가반환된다() {
+        System.out.println("생성된 캠프사이트 정보 반환 확인");
+    }
+
+    @And("수정된 캠프사이트 정보가 반환된다")
+    public void 수정된캠프사이트정보가반환된다() {
+        System.out.println("수정된 캠프사이트 정보 반환 확인");
     }
 }
--- src/test/resources/features/campsite-management.feature
+++ src/test/resources/features/campsite-management.feature
@@ -2,27 +2,18 @@ Feature: 캠프사이트 관리
   캠핑장 관리자는 캠프사이트를 조회, 등록, 수정할 수 있다.
 
   Scenario: 캠프사이트 목록을 조회한다
-    Given 다음 캠프사이트가 등록되어 있다
-      | siteNumber | description | maxPeople |
-      | A-01       | 숲 뷰, 전기가능  | 4         |
-      | A-02       | 강가, 그늘많음   | 6         |
-    When 캠프사이트 목록을 조회 요청하면 (GET "/admin/campsites")
-    Then 요청이 성공한다 (200)
-    And 응답 목록의 크기는 2이다
+    Given 캠프사이트 2개가 등록되어 있다
+    When 캠프사이트 목록을 조회한다 (GET "/admin/campsites")
+    Then 조회에 성공한다 (200)
+    And 캠프사이트 2개가 반환된다
 
   Scenario: 새로운 캠프사이트를 등록한다
-    When 다음 정보로 캠프사이트 등록 요청을 보내면 (POST "/admin/campsites")
-      | siteNumber | description    | maxPeople |
-      | B-01       | 호수 뷰, 넓은 공간 | 5         |
-    Then 리소스가 생성된다 (201)
-    And 응답의 "siteNumber" 값은 "B-01"이다
-    And 응답의 "maxPeople" 값은 5이다
+    When 새로운 캠프사이트를 등록한다 (POST "/admin/campsites")
+    Then 캠프사이트가 생성된다 (201)
+    And 생성된 캠프사이트 정보가 반환된다
 
   Scenario: 캠프사이트 정보를 수정한다
-    Given 사이트 번호 "C-01", 설명 "기본 사이트", 최대 인원 4인 캠프사이트가 등록되어 있다
-    When 해당 캠프사이트에 다음 정보로 수정 요청을 보내면 (PUT "/admin/campsites/{id}")
-      | description          | maxPeople |
-      | 리뉴얼 완료, 전기/수도 가능 | 6         |
-    Then 요청이 성공한다 (200)
-    And 응답의 "description" 값은 "리뉴얼 완료, 전기/수도 가능"이다
-    And 응답의 "maxPeople" 값은 6이다
+    Given 캠프사이트가 등록되어 있다
+    When 캠프사이트 정보를 수정한다 (PUT "/admin/campsites/{id}")
+    Then 수정에 성공한다 (200)
+    And 수정된 캠프사이트 정보가 반환된다
